9b94cf7b87697cf138a98bef3168587bd0c337a4
Alex Daddis <daddis29@gmail.com>
2020-07-13 20:04:20 -0400

    readme file has been updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,57 @@
 //Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); {
 
-//Generate Password with 8-128 characters prompt
-function generatePassword() {
+    //Generate Password with 8-128 characters prompt
+    function generatePassword() {
 
-    var pwlength = prompt("How many characters would you like your password to contain?");
+        var pwlength = prompt("How many characters would you like your password to contain?");
 
-    while (pwlength < 8 || pwlength > 128) {
-        alert("Password must be between 8-128 characters.");
-        pwlength = prompt("How many characters would you like your password to contain?");
+        while (pwlength < 8 || pwlength > 128) {
+            alert("Password must be between 8-128 characters.");
+            pwlength = prompt("How many characters would you like your password to contain?");
+        }
     }
+}
 
-    //Strings to choose password characters from
-    lowercase = "abcdefghijklmnopqrstuvwxyz";
-    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    numbers = "0123456789";
-    symbols = "!#'()*+@[_,-./:;<$%\]^&=>?`{|}~";
-    var passArray = "";
+//Strings to choose password characters from
+lowercase = "abcdefghijklmnopqrstuvwxyz";
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+numbers = "0123456789";
+symbols = "!#'()*+@[_,-./:;<$%\]^&=>?`{|}~";
+var passArray = "";
 
-    //Uppercase confirm
-    var uppercase = confirm("Would like to include uppercase letters?");
-    if (uppercase == true) {
-        passArray = passArray.concat(uppercase);
-    }
+//Uppercase confirm
+var uppercase = confirm("Would like to include uppercase letters?");
+if (uppercase == true) {
+    passArray = passArray.concat(uppercase);
+}
 
-    //Lowercase confirm
-    var lowercase = confirm("Would like to include lowercase letters?");
-    if (lowercase == true) {
-        passArray = passArray.concat(lowercase);
-    }
+//Lowercase confirm
+var lowercase = confirm("Would like to include lowercase letters?");
+if (lowercase == true) {
+    passArray = passArray.concat(lowercase);
+}
 
-    //Numbers confirm
-    var numbers = confirm("Would like to include numbers?");
-    if (numbers == true) {
-        passArray = passArray.concat(numbers);
-    }
+//Numbers confirm
+var numbers = confirm("Would like to include numbers?");
+if (numbers == true) {
+    passArray = passArray.concat(numbers);
+}
 
-    //Symbols confirm
-    var symbols = confirm("Would like to include special characters?");
-    if (symbols == true) {
-        passArray = passArray.concat(symbols);
-    }
+//Symbols confirm
+var symbols = confirm("Would like to include special characters?");
+if (symbols == true) {
+    passArray = passArray.concat(symbols);
+}
 
-    //Final password to generate
-    finalPass = " ";
-    for (var i = 0; i < length; {
-        var randomPass = passArray[Math.floor(Math.random() * passArray.length)];
-        finalPass = finalPass.concat(randomPass);
-    }
+//Final password to generate
+finalPass = " ";
+for (var i = 0; i < length; {
+    var randomPass = passArray[Math.floor(Math.random() * passArray.length)]
+            finalPass = finalPass.concat(randomPass);
+}
     return finalPass;
-};
+
 
 
 // Write password to the #password input
@@ -61,4 +63,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+document.getElementById("btn").onclick = function () {
 
+}
--- README.md
+++ README.md
@@ -1,48 +1,19 @@
-# 03 JavaScript: Password Generator
+## Password Generator 
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+An employee with access to sensitive data wants to randomly generate a password containing various criteria to create a strong password.
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-## User Story
 
 ```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+When you click the button to generate a password, you are presented with a series of prompts for password criteria.
 
-## Acceptance Criteria
+When you are prompted, you will select which criteria you’d like to include: uppercase, lowercase, special characters and numbers.
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+When you are prompted for the password, you will be informed it must be between 8-128 characters.
 
-The following image demonstrates the application functionality:
+When you choose your responses, all input will be validated as long as one character type is selected.
+When all prompts are answered, a password will be generated that matches the criteria. 
 
-![password generator demo](./Assets/03-javascript-homework-demo.png)
+When the password is generated, it is displayed in the password text box.
 
-## Review
+See the published site: https://daddis29.github.io/PasswordGenerator/
 
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
